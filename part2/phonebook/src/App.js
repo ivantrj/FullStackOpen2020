7e7ae5539b1a77092b498d7f5da26fbdf6e4c3b6
Ivan <ivantrajanovski@gmail.com>
2020-06-24 09:56:56 +0200

    countries

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Name from './components/Name'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('fullfiled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'people')
 
   const containsName = (arr, name) => {
     for (let i = 0; i < arr.length; i++) {
